cbcb1c09ffe4852e181ee88de20ad45f82051a18
93amr.kakashi <93amr.kakashi@gmail.com>
2023-04-18 21:27:05 +0700

    back end

---
+++ back/routes/blogs.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const Blog = require("../models/blogsModel");
+const { 
+  createBlog,
+  getBlogs,
+  getBlog,
+  updateBlog,
+  deleteBlog
+  
+ } = require("../controllers/blogsController");
+
+ 
+// get all blogs
+router.get('/', getBlogs);
+
+// get one blog
+router.get('/:id', getBlog);
+
+// create a blog
+router.post('/', createBlog );
+
+
+
+// delete a blog
+router.delete('/:id', deleteBlog);
+
+
+// update a blog
+router.patch('/:id', updateBlog);
+
+
+
+
+
+module.exports = router
--- back/server.js
+++ back/server.js
@@ -1,11 +1,31 @@
-const express = require('express');
-require("dotenv").config()
+require("dotenv").config();
+const express = require("express");
+const blogsRoutes = require("./routes/blogs");
+const mongoose = require("mongoose");
 
 // express app
-const app = express()
+const app = express();
 
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
 
-// listening for requests
-app.listen(process.env.PORT, () => {
-  console.log(`server started at port ${process.env.PORT}`)
-})
+  next();
+});
+
+//routes
+app.use("/api/blogs", blogsRoutes);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("connected to DB");
+    // listening for requests
+    app.listen(process.env.PORT, () => {
+      console.log(`server started at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Not connected to DB");
+  });
